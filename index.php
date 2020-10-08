e003644aa4570b31d85fa03b2e4d9fae1cc70bd5
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2020-10-08 17:03:27 +0200

    support fonction

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
+function isGroupDangerous(array $animals): bool
+{
+    foreach($animals as $animal) {
+        if(isDangerous($animal)) {
+            return true;
+        }
+    }
 
+    return false;
+}
+
+function isDangerous(array $animal): bool
+{
+    return $animal['weight'] > 50 && $animal['isCarnivorous'] === true;
+}
+
+$animals = [
+    ['species'=>'chien', 'weight'=>30, 'isCarnivorous'=> true],
+    ['species'=>'vache', 'weight'=>60, 'isCarnivorous'=> false],
+    ['species'=>'tigre', 'weight'=>100, 'isCarnivorous'=> true],
+];
+
+foreach($animals as $animal) {
+    var_dump(isDangerous($animal));
+}
+
+var_dump(isGroupDangerous($animals));
 
 ?>
